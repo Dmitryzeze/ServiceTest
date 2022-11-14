ffdd105a5d6eee4262f54819a8a4741331bd0030
Sator <lbvf1197@mail.ru>
2022-11-14 17:47:45 +0300

    begin

--- app/src/main/java/com/example/servicetest/MainActivity.kt
+++ app/src/main/java/com/example/servicetest/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.servicetest
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.servicetest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+
 }
