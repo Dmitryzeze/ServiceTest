d312a44515e6eb566edc1ecc874925aa6a26e99a
Sator <lbvf1197@mail.ru>
2022-11-23 20:52:32 +0300

    test WorkManager service

--- app/src/main/java/com/example/servicetest/MyIntentService.kt
+++ app/src/main/java/com/example/servicetest/MyIntentService.kt
@@ -1,17 +1,11 @@
 package com.example.servicetest
 
-import android.app.IntentService
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import kotlinx.coroutines.*
 
 class MyIntentService : IntentService(NAME) {
 
